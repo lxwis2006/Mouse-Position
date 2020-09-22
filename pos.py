276fa51f669fbb2db7bef9ef9c42c734a36ff477
lxwis2006 <69874257+lxwis2006@users.noreply.github.com>
2020-09-23 00:37:19 +0100

    Add files via upload

---
+++ pos.py
@@ -0,0 +1,22 @@
+import pyautogui, sys, time
+
+def start():
+    print("5")
+    time.sleep(1)
+
+    print("4")
+    time.sleep(1)
+
+    print("3")
+    time.sleep(1)
+
+    print("2")
+    time.sleep(1)
+
+    print("1")
+    time.sleep(1)
+    
+    pos = pyautogui.position()
+    print(pos)
+
+start()
